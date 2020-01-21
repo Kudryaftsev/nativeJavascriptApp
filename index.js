1a7ade1a79797b288a856536294fad8b24bb9827
Nick Kudryavtsev <58892091+th3Cactus@users.noreply.github.com>
2020-01-21 12:42:10 +0300

    Functions were realise: 1) handleBoxClick; 2) renderBox. When you start game, boxes is starting to render in static.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Потому что Node ( узел )
 var $start = document.querySelector( '#start' )
 var $game = document.querySelector( '#game' )
+
+var score = 0
+
 $start.addEventListener( 'click', startGame )
+$game.addEventListener( 'click', handleBoxClick )
 
 
 
@@ -25,7 +29,30 @@ function startGame() {
     renderBox()
 }
 
+
+function handleBoxClick( event ){
+    if ( event.target.dataset.box ){
+        score++
+        renderBox()
+    }
+}
+
 // Создаем функцию renderBox()
 function renderBox() {
+    //  Очищать поле, чтобы рендерить новые квадраты
+    $game.innerHTML = ''
 
+    // make element in js
+    var box = document.createElement( 'div' )
+    box.style.height = box.style.width = '50px'     //  later - dynamic generation
+    box.style.position = 'absolute'                 //  для позиционирования в рамках квадрата
+    box.style.backgroundColor = 'black'             //  later - dynamic generation
+    box.style.top = '50px'                          //  later - dynamic...
+    box.style.left = '70px'                         //  later - dynamic...
+    box.style.cursor = 'pointer'
+    // да, это выдуманный атрибут, крута (только без префикса data)
+    box.setAttribute( 'data-box', 'true' )
+
+    //  mdn - 1) beforebegin; 2) afterbegin; 3) beforeend; 4) afterend - до элемента, сразу после откр тега и так далее, второй параметр - сам элемент
+    $game.insertAdjacentElement( 'afterbegin', box )
 }
