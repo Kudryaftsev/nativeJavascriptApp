48da8ce7e07df44dd8e7fbc44c3ba5fadabd12cf
Nick Kudryavtsev <58892091+th3Cactus@users.noreply.github.com>
2020-01-21 13:22:13 +0300

    interval is realised; disabled after time out

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Потому что Node ( узел )
 var $start = document.querySelector( '#start' )
 var $game = document.querySelector( '#game' )
+var $time = document.querySelector( '#time' )
 
 var score = 0
+var isGameStarted = false
 
 $start.addEventListener( 'click', startGame )
 $game.addEventListener( 'click', handleBoxClick )
@@ -21,16 +23,37 @@ $game.addEventListener( 'click', handleBoxClick )
 
 
 function startGame() {
+    isGameStarted = true
     // Скрываем кнопку и определяем фон
     $start.classList.add( 'hide' )
     $game.style.backgroundColor = '#fff'
+
+    var interval = setInterval( function(){
+        var time = parseFloat($time.textContent)
+
+        if (time <= 0){
+            clearInterval( interval )
+            endGame()
+        } else {
+            $time.textContent = (time - 0.1).toFixed( 1 )
+        }
+    }, 100 )
 
     // При старте игры - вызываем эту функцию
     renderBox()
 }
 
+function endGame(){
+    isGameStarted = false
+}
+
 
 function handleBoxClick( event ){
+    // т.е. если false, почему бы и нет?
+    if (!isGameStarted){
+        return
+    }
+
     if ( event.target.dataset.box ){
         score++
         renderBox()
@@ -51,7 +74,7 @@ function renderBox() {
     var box = document.createElement( 'div' )
     box.style.height = box.style.width = boxSize + 'px' //  dynamic generation
     box.style.position = 'absolute'                     //  для позиционирования в рамках квадрата
-    box.style.backgroundColor = 'black'                 //  later - dynamic generation
+    box.style.backgroundColor = 'rgb(' + getRandom( 0, 256) + ',' + getRandom( 0, 256) + ',' + getRandom( 0, 256 ) + ')'                                                 //  backgroundColor - dynamic generation
     box.style.top = getRandom( 0, maxTop ) + 'px'       //  dynamic...
     box.style.left = getRandom( 0, maxLeft ) + 'px'     //  dynamic...
     box.style.cursor = 'pointer'
